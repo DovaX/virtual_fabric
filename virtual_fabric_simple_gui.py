9fa83c2b14df157f55ddfc1c210346188496782a
Dominik Vach <vach.dominik@gmail.com>
2022-06-16 23:58:32 +0200

    initialize simple gui

---
+++ virtual_fabric_simple_gui.py
@@ -0,0 +1,14 @@
+
+
+import dogui.dogui_core as dg
+
+
+gui1=dg.GUI()
+
+
+#dg.Label(gui1.window, text_input, row_index, column_index)
+#dg.Entry(gui1.window, row_index, column_index)
+#dg.Button(gui1.window, text_input, function, row_index, column_index)
+
+
+gui1.build_gui()
