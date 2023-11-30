98e7e863c60ec2e50151db3206e59bce59cd8a68
Dominik Vach <vach.dominik@gmail.com>
2023-11-30 23:59:46 +0100

    generate package + release 0.1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='virtual_fabric',
+    version='0.1.0',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='High level library used for smooth orchestration of virtual machines using fabric
+
+Goal of the package: Easy usage of Git, Python, SCP and Crontab on Virtual machines/Remote servers using only Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DovaX/virtual_fabric',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'fabric'
+     ],
+    python_requires='>=3.6',
+)
+    
